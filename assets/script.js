253eb283b1713b417cd241f1c269db6d0949c39c
_blockaye_ <blake_austinjohnson@icloud.com>
2021-03-26 16:10:09 -0500

    assist

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,107 @@
+var locationCoordinates = [];
 
-var cityCurrent = { };
 
-var getCurrentWeather = function (zip) {
+var getLocationWeather = function (lat, lon) {
+    var newApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=&appid=d0f4ff36139913f4de0b35a854f44600`;
 
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&appid=d0f4ff36139913f4de0b35a854f44600";
-
-    fetch(apiUrl)
-    .then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                console.log(data);
-                displayWeather(data, zip);
-            });
-        } else {
-            alert("Error: " + response.statusText);
+    fetch(newApiUrl).then(function (newRespone) {
+    if (newRespone.ok) {
+        newRespone.json().then(function (newData) {
+        console.log(newData);
+        });
         }
+    }
+)};
+
+var getCoordinates = function (cityName, stateCode) {
+  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode}&appid=d0f4ff36139913f4de0b35a854f44600`;
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+            var lat = data.coord.lat;
+            console.log(lat); 
+            var lon = data.coord.lon;
+            console.log(lon);
+            getLocationWeather(lat, lon);
+        })
+      } else {
+        alert("Error: " + response.statusText);
+      }
     })
         .catch(function (error) {
         alert("Unable to connect to Open Weather");
-    });
-};
-
-var cityNameEl = document.getElementById("cityName");
-var tempEl = document.getElementById("temp");
-
-
-
-var searchHandler = function(event) {
-    event.preventDefualt();
-
-    var searchInputEl = document.getElementById("search");
-    console.log(searchInputEl);
-
-    var searchInput = searchInputEl.value.trim();
-    console.log(searchInput);
-
-    if (searchInput) {
-        getCurrentWeather(searchInput);
-        searchInputEl.value = "";
-    } else {
-        alert("please enter a city.");
-    }
-};
-
-
-//////////////
-var displayWeather = function (currentWeather) {
-
-
+        });
 
 };
-//////////////
+
+//console.log(locationCoordinates);
 
 
 
-getCurrentWeather(78748);
+
+
+
+
+
+
+
+
+////////////////////
+// var uvIndexEl = document.getElementById("uv-index");
+// uvIndexEl.textContent = currentWeather.
+/////////////////////////
+
+
+
+
+
+var searchHandler = function (event) {
+  event.preventDefualt();
+
+  var searchInputEl = document.getElementById("search");
+  console.log(searchInputEl);
+
+  var searchInput = searchInputEl.value.trim();
+  console.log(searchInput);
+
+  if (searchInput) {
+    getCurrentWeather(searchInput);
+    searchInputEl.value = "";
+  } else {
+    alert("please enter a city.");
+  }
+};
+
+var displayWeather = function (currentWeather, location) {
+  console.log(currentWeather, location);
+
+  var cityNameEl = document.getElementById("cityName");
+  cityNameEl.textContent = currentWeather.name;
+
+  var tempEl = document.getElementById("temp");
+  tempEl.textContent = "Temp: " + currentWeather.main.temp;
+
+  var humidityEl = document.getElementById("humidity");
+  humidityEl.textContent = "Humidity: " + currentWeather.main.humidity;
+
+  var windSpeedEl = document.getElementById("wind");
+  windSpeedEl.textContent = "Wind Speed: " + currentWeather.wind.speed;
+};
+////////////
+
+
+
+getCoordinates("Austin, TX")
+
+
+
+
+
+
+
+    
+//getLocationWeather();
+
 
